cace9b2b0912ea7367bf3d37c7362f549c0bbec1
KobirBappy <kobirhossen115@gmail.com>
2022-11-08 21:02:09 +0600

    NewUpdate

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 //import 'package:facebook_audience_network/facebook_audience_network.dart';
 import 'package:flutter/material.dart';
 //import 'package:google_mobile_ads/google_mobile_ads.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
+// import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'page/splash_home.dart';
 
 void main() {
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+ // WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   //MobileAds.instance.initialize();
 
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+ // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   // FacebookAudienceNetwork.init(
   //   testingId: "328404cebf50ec1fdb05795c0007a8a7", //test
   //   //testingId: "1096060697976196", //orginal
@@ -30,3 +30,24 @@ class SehariApp extends StatelessWidget {
     );
   }
 }
+
+// class splashpage extends StatefulWidget {
+//   const splashpage({Key? key}) : super(key: key);
+//
+//   @override
+//   State<splashpage> createState() => _splashpageState();
+// }
+//
+// class _splashpageState extends State<splashpage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       height: 300,
+//       width: 400,
+//       color: Colors.brown,
+//       child: FlutterLogo(),
+//
+//     );
+//   }
+// }
+
--- lib/page/splash_home.dart
+++ lib/page/splash_home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
+// import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'facebookadd.dart';
 import 'google_add.dart';
@@ -14,34 +14,34 @@ class SplashScreen extends StatefulWidget {
 
 // ignore: camel_case_types
 class _splashScreenState extends State<SplashScreen> {
-  static const String _url =
-      "https://play.google.com/store/apps/developer?id=Lambda+Technologies";
-  void _launchURL() async {
-    // ignore: deprecated_member_use
-    if (!await launch(_url)) throw 'Could not launch $_url';
-  }
+  // static const String _url =
+  //     "https://play.google.com/store/apps/developer?id=Lambda+Technologies";
+  // void _launchURL() async {
+  //   // ignore: deprecated_member_use
+  //   if (!await launch(_url)) throw 'Could not launch $_url';
+  // }
 
-  @override
-  void initState() {
-    super.initState();
-    initialization();
-  }
+  //  @override
+  // void initState() {
+  //   super.initState();
+  //   initialization();
+  // }
 
-  void initialization() async {
-    // This is where you can initialize the resources needed by your app while
-    // the splash screen is displayed.  Remove the following example because
-    // delaying the user experience is a bad design practice!
-    // ignore_for_file: avoid_print
-    print('ready in 3...');
-    await Future.delayed(const Duration(seconds: 1));
-    print('ready in 2...');
-    await Future.delayed(const Duration(seconds: 1));
-    print('ready in 1...');
-    await Future.delayed(const Duration(seconds: 1));
-    print('go!');
-    FlutterNativeSplash.remove();
-    // openingAd();
-  }
+  // void initialization() async {
+  //   // This is where you can initialize the resources needed by your app while
+  //   // the splash screen is displayed.  Remove the following example because
+  //   // delaying the user experience is a bad design practice!
+  //   // ignore_for_file: avoid_print
+  //   // print('ready in 3...');
+  //   // await Future.delayed(const Duration(seconds: 1));
+  //   // print('ready in 2...');
+  //   // await Future.delayed(const Duration(seconds: 1));
+  //   // print('ready in 1...');
+  //   // await Future.delayed(const Duration(seconds: 1));
+  //   // print('go!');
+  //   //FlutterNativeSplash.remove();
+  //   // openingAd();
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -179,7 +179,7 @@ class _splashScreenState extends State<SplashScreen> {
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
                             GestureDetector(
-                              onTap: _launchURL,
+                             // onTap: _launchURL,
                               child: Container(
                                 height: 50,
                                 width: 170,
